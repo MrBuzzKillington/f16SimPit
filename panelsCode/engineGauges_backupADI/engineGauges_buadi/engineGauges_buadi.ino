e9177c0e7c8843c46146796ed51acccce2bf2421
buzzKillington <thestruuus@gmail.com>
2022-10-20 23:14:42 -0400

    added fixes to engine gauges and created library to f x168 driven by easydriver chip

--- panelsCode/engineGauges_backupADI/engineGauges_buadi/engineGauges_buadi.ino
+++ panelsCode/engineGauges_backupADI/engineGauges_buadi/engineGauges_buadi.ino
@@ -2,10 +2,9 @@
  EnginePanels
  */
 #define DCSBIOS_IRQ_SERIAL
-#include <Servo.h>
-#include <Stepper.h>
 #include <SPI.h>
 #include <Wire.h>
+#include "x168EasyDriver.h"
 
 
 #include "DcsBios.h"
@@ -34,21 +33,21 @@ int NozOpenCurStp_ = 0;
 int rpmCurStp_ = 0;
 int FtitCurStp_ = 0;
 
-Stepper OilStepper_(x27MaxStep_, 29, 27, 25, 23);
-Stepper NozStepper_(x27MaxStep_, 37, 35, 33, 31);
-Stepper RpmStepper_(x27MaxStep_, 28, 26, 24, 22);
-Stepper FtitStepper_(x27MaxStep_,36, 34, 32, 30);
+x168EasyDriver OilStepper_(22,24);
+x168EasyDriver NozStepper_(26,28);
+x168EasyDriver RpmStepper_(30,32);
+x168EasyDriver FtitStepper_(34,36);
 
 
 void onEngineOilPressureChange(unsigned int newValue)
 {
     //int newPos = map(newValue, 0, 65535, 0, x27MaxStep_);
     long val100 = 51310; //0, 51000, 0, 466
-    long volStps = 500;//340;
+    long volStps = 1000;//340;
     int newPos = map(newValue, 0, val100, 0, volStps);
     if (newPos != oilPressureCurStp_)
     {
-        oilPressureCurStp_ = StepTo( OilStepper_ , oilPressureCurStp_, newPos);  
+        oilPressureCurStp_ = OilStepper_.gotoPosition( newPos );  
     }
 }
 DcsBios::IntegerBuffer engineOilPressureBuffer(0x44da, 0xffff, 0, onEngineOilPressureChange);
@@ -57,11 +56,11 @@ DcsBios::IntegerBuffer engineOilPressureBuffer(0x44da, 0xffff, 0, onEngineOilPre
 void onEngineNozzlePositionChange(unsigned int newValue) 
 {
     long valNoz100 = 58588; //0, 51000, 0, 466
-    long volNozStps = 466;
+    long volNozStps = 932;
     int newPos = map(newValue, 0, valNoz100, 0, volNozStps);
     if (newPos != NozOpenCurStp_)
     {
-        NozOpenCurStp_ = StepTo( NozStepper_ , NozOpenCurStp_, newPos);  
+        NozOpenCurStp_ = NozStepper_.gotoPosition( newPos );  
     }
 }
 DcsBios::IntegerBuffer engineNozzlePositionBuffer(0x44dc, 0xffff, 0, onEngineNozzlePositionChange);
@@ -71,11 +70,11 @@ void onEngineTachometerChange(unsigned int newValue)
 {
     //int newPos = map(newValue, 0, 65535, 0, x27MaxStep_);
     long val100 = 52519; //0, 51000, 0, 466
-    long volStps = 500;//340;
+    long volStps = 1000;//340;
     int newPos = map(newValue, 0, val100, 0, volStps);
     if (newPos != rpmCurStp_)
     {
-        rpmCurStp_ = StepTo( RpmStepper_ , rpmCurStp_, newPos);  
+        rpmCurStp_ = RpmStepper_.gotoPosition( newPos);  
     }
 }
 DcsBios::IntegerBuffer engineTachometerBuffer(0x44de, 0xffff, 0, onEngineTachometerChange);
@@ -85,11 +84,11 @@ void onEngineFtitChange(unsigned int newValue)
 {
     //int newPos = map(newValue, 0, 65535, 0, x27MaxStep_);
     long val100 = 35400; //0, 51000, 0, 466
-    long volStps = 325;//340;
+    long volStps = 650;//340;
     int newPos = map(newValue, 0, val100, 0, volStps);
     if (newPos != FtitCurStp_)
     {
-        FtitCurStp_ = StepTo( FtitStepper_ , FtitCurStp_, newPos);  
+        FtitCurStp_ = FtitStepper_.gotoPosition( newPos );  
     }
 }
 DcsBios::IntegerBuffer engineFtitBuffer(0x44e0, 0xffff, 0, onEngineFtitChange);
@@ -127,16 +126,18 @@ void onMaxPwrSwChange(unsigned int newValue)
       PollAllControls();
 
       //Reset the steppers
-      oilPressureCurStp_ = HomeStepper(OilStepper_); //Home the stepper   
-      NozOpenCurStp_ = HomeStepper(NozStepper_); //Home the stepper 
-      rpmCurStp_ = HomeStepper(RpmStepper_); //Home the stepper     
-      FtitCurStp_ = HomeStepper(FtitStepper_); //Home the stepper     
+      oilPressureCurStp_ = OilStepper_.homeStepper(); //Home the stepper   
+      NozOpenCurStp_ = NozStepper_.homeStepper(); //Home the stepper 
+      rpmCurStp_ = RpmStepper_.homeStepper(); //Home the stepper     
+      FtitCurStp_ = FtitStepper_.homeStepper(); //Home the stepper     
     } 
     if (newValue == 0)
     {
       oldMaxPwrState = 0;
     }
 }
+
+
 DcsBios::IntegerBuffer maxPwrSwBuffer(0x4424, 0x0008, 3, onMaxPwrSwChange);
 
 DcsBios::LED lightEngFire(0x447a, 0x0008, 10);
@@ -154,6 +155,13 @@ DcsBios::LED lightDisc(0x447c, 0x0010, 14);
 DcsBios::LED lightArNws(0x447c, 0x0008, 15);
 DcsBios::LED lightRdy(0x447c, 0x0004, 16);
 
+
+DcsBios::RotaryEncoder saiPitchTrim("SAI_PITCH_TRIM", "-500", "+500", 19, 18);
+DcsBios::ActionButton saiCageToggle("SAI_CAGE", "TOGGLE", 17);
+//DcsBios::Switch2Pos saiCage("SAI_CAGE", 17);
+
+
+
 /*
  * 
  *   MAIN FUNCTIONS
@@ -167,19 +175,22 @@ DcsBios::LED lightRdy(0x447c, 0x0004, 16);
 void setup() 
 {
 
-  setupSteppers();
+  oilPressureCurStp_ = OilStepper_.homeStepper(); //Home the stepper   
+  NozOpenCurStp_ = NozStepper_.homeStepper(); //Home the stepper 
+  rpmCurStp_ = RpmStepper_.homeStepper(); //Home the stepper     
+  FtitCurStp_ = FtitStepper_.homeStepper(); //Home the stepper  
 
 
- pinMode(4, OUTPUT);
- pinMode(5, OUTPUT);
- pinMode(6, OUTPUT);
- pinMode(7, OUTPUT);
- pinMode(8, OUTPUT);
- pinMode(9, OUTPUT);
- pinMode(10, OUTPUT);
- pinMode(11, OUTPUT);
- pinMode(12, OUTPUT);
- pinMode(13, OUTPUT);
+   pinMode(4, OUTPUT);
+   pinMode(5, OUTPUT);
+   pinMode(6, OUTPUT);
+   pinMode(7, OUTPUT);
+   pinMode(8, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(10, OUTPUT);
+   pinMode(11, OUTPUT);
+   pinMode(12, OUTPUT);
+   pinMode(13, OUTPUT);
  
   powerOnTest();
 
@@ -233,46 +244,17 @@ void powerOnTest()
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(100);     
 
-  
-//    //HomeStepper(OilStepper_); //Home the stepper
-    StepTo( OilStepper_, 0 , x27MaxStep_);
-    oilPressureCurStp_ = HomeStepper(OilStepper_); //Home the stepper  
 
+  oilPressureCurStp_ = OilStepper_.maxStepper( );
+  NozOpenCurStp_ = NozStepper_.maxStepper(  );
+  rpmCurStp_ = RpmStepper_.maxStepper( );
+  FtitCurStp_ = FtitStepper_.maxStepper( );
 
-
+  oilPressureCurStp_ = OilStepper_.homeStepper(); //Home the stepper   
+  NozOpenCurStp_ = NozStepper_.homeStepper(); //Home the stepper 
+  rpmCurStp_ = RpmStepper_.homeStepper(); //Home the stepper     
+  FtitCurStp_ = FtitStepper_.homeStepper(); //Home the stepper  
  
-    //nozzle
-    //HomeStepper(NozStepper_); //Home the stepper
-    StepTo( NozStepper_, 0 , x27MaxStep_);
-    NozOpenCurStp_ = HomeStepper(NozStepper_); //Home the stepper 
- 
-//    //RPM
-//    //HomeStepper(RpmStepper_); //Home the stepper
-    StepTo( RpmStepper_, 0 , x27MaxStep_);
-    rpmCurStp_ = HomeStepper(RpmStepper_); //Home the stepper
-//    
-//    //FTIT
-    //HomeStepper(FtitStepper_); //Home the stepper
-    StepTo( FtitStepper_, 0 , x27MaxStep_);
-    FtitCurStp_ = HomeStepper(FtitStepper_); //Home the stepper
-
-
-
-//    delay(500);
-//    oilPressureCurStp_ = StepTo( OilStepper_, oilPressureCurStp_ , map(35026, 0, 65535, 0, x27MaxStep_)) ;//should be 55%
-//    delay(500);
-//    oilPressureCurStp_ = StepTo( OilStepper_, oilPressureCurStp_ , map(16284, 0, 65535, 0, x27MaxStep_)) ;//should be 35026 25%
-//    delay(500);
-//    oilPressureCurStp_ = StepTo( OilStepper_, oilPressureCurStp_ , map(0, 0, 65535, 0, x27MaxStep_)) ; 
-
-
-//    long maxNozScale = 65535;
-//    delay(500);
-//    NozOpenCurStp_ = StepTo( NozStepper_, NozOpenCurStp_ , map(51000, 0, 51000, 0, 466)) ;//should be 100%
-//    delay(500);
-//    NozOpenCurStp_ = StepTo( NozStepper_, NozOpenCurStp_ , map(20000, 0, maxNozScale, 0, x27MaxStep_)) ;//should be 22731 40%
-//    delay(500);
-//    NozOpenCurStp_ = StepTo( NozStepper_, NozOpenCurStp_ , map(0, 0, maxNozScale, 0, x27MaxStep_)) ; 
 
 }
 
